db26ca09fd17a014daf4f110804a6bae6f9c1d84
xlLucas31 <lamagiadetodo31@gmail.com>
2025-06-30 01:53:50 -0300

    Guardar

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-const { connect } = require('./db');
 const gEarthManager = require('./g-earth-manager');
 
 async function main() {
-  const db = await connect();
-  const collection = db.collection('users');
   await gEarthManager.init();
 }
 
